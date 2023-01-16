3a96cec0b138a15c18a5f577fb5a101ac9772f1e
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-01-16 15:14:08 -0300

    Adicionado recurso visual Js funcionamento

--- js/script.js
+++ js/script.js
@@ -65,3 +65,22 @@ function eventosGaleria(img) {
 }
 
 galeria.forEach(eventosGaleria);
+
+// Horario funcionamento aberto ou fechado
+
+const funcionamento = document.querySelector("[data-semana]");
+const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+
+const dataAgora = new Date();
+const diaAgora = dataAgora.getDay();
+const horarioAgora = dataAgora.getHours();
+
+const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+
+const horarioAberto =
+  horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+
+if (semanaAberto && horarioAberto) {
+  funcionamento.classList.add("aberto");
+}
